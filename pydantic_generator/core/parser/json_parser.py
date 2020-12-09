63f7970ab7a2ae2b9fa110ed6a8ca52c102aad4d
rhoboro <rhoboro@gmail.com>
2020-12-09 21:11:15 +0900

    feat(parser): refactoring

--- pydantic_generator/core/parser/json_parser.py
+++ pydantic_generator/core/parser/json_parser.py
@@ -1,12 +1,14 @@
 import ast
 import json
 from ast import *
-from typing import IO, Iterable, Tuple, Union
+from typing import IO, Iterable, Iterator, Union
 
 from .base import Parser
 
 # Supported types in https://docs.python.org/ja/3/library/json.html#json.JSONEncoder
-JsonValue = Union[dict, list, int, float, str, bool, None]
+CollectionValue = Union[dict, list]
+PrimitiveValue = Union[bool, int, float, str]
+JsonValue = Union[CollectionValue, PrimitiveValue]
 
 
 class JsonParser(Parser):
@@ -18,102 +20,29 @@ class JsonParser(Parser):
         return list(class_parse(self.name, self.body))
 
 
-def class_parse(key: str, json_value: JsonValue) -> Iterable[ast.ClassDef]:
+def class_parse(key: str, json_value: JsonValue) -> Iterator[Union[ClassDef, AnnAssign]]:
     if isinstance(json_value, dict):
-        yield ast.ClassDef(
-            name=key.capitalize(),
-            bases=[Name(id="BaseModel", ctx=Load())],
-            keywords=[],
-            body=list(list(class_parse(k, v)) for k, v in json_value.items()),
-            decorator_list=[],
-        )
-        yield AnnAssign(
-            target=Name(id=key, ctx=Store()),
-            annotation=Name(id=key.capitalize(), ctx=Load()),
-            simple=1,
-        )
+        yield from process_dict(key, json_value)
     elif isinstance(json_value, list):
-        if json_value:
-            if isinstance(json_value[0], dict):
-                yield ast.ClassDef(
-                    name=key.capitalize(),
-                    bases=[Name(id="BaseModel", ctx=Load())],
-                    keywords=[],
-                    body=list(list(class_parse(k, v)) for k, v in json_value[0].items()),
-                    decorator_list=[],
-                )
-                yield AnnAssign(
-                    target=Name(id=key, ctx=Store()),
-                    annotation=Subscript(
-                        value=Name(id="list", ctx=Load()),
-                        slice=Name(id=key.capitalize(), ctx=Load()),
-                        ctx=Load(),
-                    ),
-                    simple=1,
-                )
-            elif isinstance(json_value[0], list):
-                value, nest = detect(json_value[0])
-                if isinstance(value, dict):
-                    yield ast.ClassDef(
-                        name=key.capitalize(),
-                        bases=[Name(id="BaseModel", ctx=Load())],
-                        keywords=[],
-                        body=list(list(class_parse(k, v)) for k, v in value.items()),
-                        decorator_list=[],
-                    )
-                    yield AnnAssign(
-                        target=Name(id=key, ctx=Store()),
-                        annotation=nested_element(key, key.capitalize(), nest),
-                        simple=1,
-                    )
-                elif isinstance(value, list):
-                    yield AnnAssign(
-                        target=Name(id=key, ctx=Store()),
-                        annotation=nested_element(key, list.__name__, nest),
-                        simple=1,
-                    )
-
-                elif isinstance(value, (bool, int, float, str)):
-                    yield AnnAssign(
-                        target=Name(id=key, ctx=Store()),
-                        annotation=nested_element(key, value.__class__.__name__, nest),
-                        simple=1,
-                    )
-
-            elif isinstance(json_value[0], (bool, int, float, str)):
-                yield AnnAssign(
-                    target=Name(id=key, ctx=Store()),
-                    annotation=Subscript(
-                        value=Name(id="list", ctx=Load()),
-                        slice=Name(id=json_value[0].__class__.__name__, ctx=Load()),
-                        ctx=Load(),
-                    ),
-                    simple=1,
-                )
-        else:
-            yield AnnAssign(
-                target=Name(id=key, ctx=Store()), annotation=Name(id="list", ctx=Load()), simple=1
-            )
-
+        yield from process_list(key, json_value)
     elif isinstance(json_value, (bool, int, float, str)):
-        yield AnnAssign(
-            target=Name(id=key, ctx=Store()),
-            annotation=Name(id=json_value.__class__.__name__, ctx=Load()),
-            simple=1,
-        )
+        yield from process_primitive(key, json_value)
 
 
-def detect(value: JsonValue, nest: int = 0) -> Tuple[JsonValue, int]:
+def get_first_and_nest_count(value: JsonValue, nest: int = 0) -> tuple[JsonValue, int]:
+    """リスト内の最初のリスト以外の値と入れ子の数を返す"""
+
     if not isinstance(value, list):
         return value, nest
     else:
         if value:
-            return detect(value[0], nest + 1)
+            return get_first_and_nest_count(value[0], nest + 1)
         else:
             return value, nest
 
 
 def nested_element(key: str, type_name: str, nest: int) -> Subscript:
+    """nestの数だけlist[]を付与したast.ASTを返す"""
     if nest:
         return Subscript(
             value=Name(id="list", ctx=Load()),
@@ -125,4 +54,57 @@ def nested_element(key: str, type_name: str, nest: int) -> Subscript:
             value=Name(id="list", ctx=Load()),
             slice=Name(id=type_name, ctx=Load()),
             ctx=Load(),
+        )
+
+
+def process_dict(key: str, value: dict, nest: int = 0) -> Iterator[Union[ClassDef, AnnAssign]]:
+    yield ast.ClassDef(
+        name=key.capitalize(),
+        bases=[Name(id="BaseModel", ctx=Load())],
+        keywords=[],
+        body=list(list(class_parse(k, v)) for k, v in value.items()),
+        decorator_list=[],
+    )
+
+    if nest:
+        yield AnnAssign(
+            target=Name(id=key, ctx=Store()),
+            annotation=nested_element(key, key.capitalize(), nest),
+            simple=1,
+        )
+    else:
+        yield AnnAssign(
+            target=Name(id=key, ctx=Store()),
+            annotation=Name(id=key.capitalize(), ctx=Load()),
+            simple=1,
+        )
+
+
+def process_list(key: str, value: list, nest: int = 0) -> Iterable[Union[ClassDef, AnnAssign]]:
+    if not value:
+        yield AnnAssign(
+            target=Name(id=key, ctx=Store()), annotation=Name(id="list", ctx=Load()), simple=1
+        )
+        raise StopIteration
+
+    if isinstance(value[0], list):
+        yield from process_list(key, value[0], nest=nest + 1)
+    if isinstance(value[0], dict):
+        yield from process_dict(key, value[0], nest=nest)
+    elif isinstance(value[0], (bool, int, float, str)):
+        yield from process_primitive(key, value[0], nest=nest)
+
+
+def process_primitive(key: str, value: PrimitiveValue, nest: int = 0) -> Iterator[AnnAssign]:
+    if nest:
+        yield AnnAssign(
+            target=Name(id=key, ctx=Store()),
+            annotation=nested_element(key, value.__class__.__name__, nest),
+            simple=1,
+        )
+    else:
+        yield AnnAssign(
+            target=Name(id=key, ctx=Store()),
+            annotation=Name(id=value.__class__.__name__, ctx=Load()),
+            simple=1,
         )
